08fb2db11fb9f7269fb45fa6b638a78f4b283579
sam-lev <samlev@cs.utah.edu>
2020-03-03 15:04:15 -0700

    wrote graph embedder with trained mscgnn and inference class using graph embedder and linear regression on unknown geomsc

--- topoml/topoml/topology/geometric_msc.py
+++ topoml/topoml/topology/geometric_msc.py
@@ -194,6 +194,8 @@ class GeoMSC:
 
         if original_image.shape[0] == 3:
             mapped_image = np.transpose(original_image, (1, 2, 0))#original_image.shape[2]))
+        elif original_image.shape[1] == 3:
+            mapped_image = np.transpose(original_image, (0, 2, 1))
         else:
             mapped_image = original_image
         mapped_image *= 255
