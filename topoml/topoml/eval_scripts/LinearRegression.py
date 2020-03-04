b093e60a6e951d87a2d6a0017107a417fc1151f0
sam-lev <samlev@cs.utah.edu>
2020-03-03 17:23:06 -0700

    wrote graph embedder with trained mscgnn and inference class using graph embedder and linear regression on unknown geomsc

--- topoml/topoml/eval_scripts/LinearRegression.py
+++ topoml/topoml/eval_scripts/LinearRegression.py
@@ -128,9 +128,9 @@ class LinearRegression:
             if self.mscgnn_infer is not None:
                 for id, arc in zip(test_ids, self.mscgnn_infer.arcs):
                     pred = log.predict_proba(embeds[[id_map[id]]])
-                    print("pred shape: ", pred.shape)
+                    print("pred : ", pred)
                     pred = pred[0]
-                    print("pred", pred)
+                    print("pred0", pred)
                     arc.label_accuracy =  float(pred[1])#[0])#[int(pred[0]),int(pred[1])]
 
             pred_path = os.path.join(self.embedding_path.split("/")[:-1][0], self.embedding_path.split("/")[:-1][1],self.embedding_path.split("/")[:-1][2],'predicted_graph-G.json')
